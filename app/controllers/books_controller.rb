f2333906d1ea2f157584b928ea3200366c3b7e3b
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2020-12-18 15:33:30 +0000

    [Update]ユーザー名でのログインに変更

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,27 @@
+class BooksController < ApplicationController
+  
+  def create
+  end
+  
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+    e
+  
+end
