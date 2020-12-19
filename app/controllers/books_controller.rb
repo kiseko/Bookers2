fd0111becfc13fc31c4f7f316333f37ea5ba81c5
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2020-12-19 06:44:29 +0000

    [Update]投稿機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,20 @@
 class BooksController < ApplicationController
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def index
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
