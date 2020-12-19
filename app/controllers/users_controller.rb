dbf79e80e7c0098576541c84eec71a9f488f640d
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2020-12-19 04:42:52 +0000

    [Update]ユーザー情報編集機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
   def index
+    @user = current_user
+    @users = User.all
   end
 
   def show
@@ -8,9 +10,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f32dad1ebc7ed72fd00eb6680f5892f7e9adc37b34158d201a37569fcf8c70ba2174ad43be6def70fbbfe5fdbfd99af7b367c6baca14d99e71c459cb3b424b77'
